7e21586b604f478ca399efacbe0b0432113296ef
Sukma Wijaya Saputra <coklatpanas83@gmail.com>
2020-11-27 13:39:40 +0700

    add post to user

---
+++ app/models/master.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from app.extensions._db import db
+
--- app/views/auths.py
+++ app/views/auths.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request
 from app.forms.registers import RegisterForm
+from app.models.users import UsersModel
+from app.extensions._db import db
 
 bp = Blueprint('auth', __name__)
 
@@ -9,9 +11,13 @@ def register():
     form = RegisterForm()
 
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
+        user = UsersModel(
+           username = request.form['username'],
+           full_name = request.form['password'],
+           email = request.form['email'],
+           password = request.form['password']
+        )
+        db.session.add(user)
+        db.session.commit()
 
     return render_template('auths/register.html', form=form)
